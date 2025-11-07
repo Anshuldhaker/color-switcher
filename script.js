b86372ea468f4cbcfe7424ab989758fc0e7c3ce1
Anshul Dhaker <anshuldhaker9@gmail.com>
2025-11-07 22:44:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+ const buttons=document.querySelectorAll('.button');
+ const body=document.querySelector("body");
+
+ buttons.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+  if(e.target.id==='grey'){
+    body.style.backgroundColor="grey"
+  }
+  if(e.target.id==='green'){
+    body.style.backgroundColor="green"
+  }
+  if(e.target.id==='yellow'){
+    body.style.backgroundColor="yellow"
+  }
+  if(e.target.id==='red'){
+    body.style.backgroundColor="red"
+  }
+  if(e.target.id==='orange'){
+    body.style.backgroundColor="orange"
+  }
+    })
+ })
